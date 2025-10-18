74db661e680f3f6d7f0bc72333a6df47229dfe36
Raktim-b <raktimb414@gmail.com>
2025-10-18 17:02:56 +0530

    swiper added

---
+++ script/script.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const swiper = new Swiper(".swiper", {
+    slidesPerView: 1,
+    centeredSlides: true,
+    freeMode: true,
+    // autoplay: {
+    //   delay: 1500,
+    //   disableOnInteraction: false,
+    // },
+    loop: true,
+    breakpoints: {
+      576: {
+        slidesPerView: 2,
+      },
+
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 50,
+      },
+    },
+  });
+});
